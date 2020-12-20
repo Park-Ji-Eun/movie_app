873078de32100e0eba002053a26799b06c2e3cbb
Park-Ji-Eun <penny0123@naver.com>
2020-12-20 23:15:22 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React from 'react';
 
-function Fruit({fav}) {
+function Fruit({name, picture}) {
   return (
-    <h1>I like {fav}!!</h1>
-  );
-}
-
-function App() {
-  return (
-    <div> 
-      <h1>Hello!</h1>
-      <Fruit fav = "apple"/>
-      <Fruit fav = "strawberry"/>
-      <Fruit fav = "peach"/>
-      <Fruit fav = "orange"/>
-      <Fruit fav = "grape"/>
+    <div>
+      <h2>I like {name}!</h2>
+      <img src={picture} />
     </div>
   );
 }
 
+const fruitILike = [
+  {
+    name: "apple",
+    image: "http://1.bp.blogspot.com/_cYoDgrIrq5Q/TKjwPFAQxuI/AAAAAAAAAD8/akB4ZcrxWE4/s1600/Red+Apple+XSmall.jpg"
+  },
+  {
+    name : "strawberry",
+    image : "https://theflavorcompany.com/wp-content/uploads/2018/03/Strawberry.jpg"
+  },
+  {
+    name : "peach",
+    image : "https://befreshcorp.net/wp-content/uploads/2017/07/product-packshot-nactarine.jpg"
+  },
+  {
+    name : "orange",
+    image : "https://southernhospitalityblog.com/wp-content/uploads/2011/07/orange.jpg"
+  },
+  {
+    name : "grape",
+    image : "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/263/263332/three-bunches-of-grapes.jpg?w=1155&h=978"
+  }
+]
+
+function App() {
+  return (
+    <div>
+      {fruitILike.map(fruits => (
+        <Fruit name={fruits.name} picture={fruits.image} />
+      ))}
+    </div>
+  );
+}
+
+/*
+function App() {
+  return (
+    <div>
+      {fruitILike.map(function(fruits) {    // 함수로 선언해도 같은 결과 나옴
+        return <Fruit name={fruits.name} picture={fruits.image} />
+      })}
+    </div>
+  )
+}
+*/
 export default App;
